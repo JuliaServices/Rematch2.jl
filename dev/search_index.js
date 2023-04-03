c9890dcf85335f7cbd187b46e292ebd942d63cde
Documenter.jl <documenter@juliadocs.github.io>
2023-04-03 19:18:15 +0000

    build based on 9eb5e6e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Rematch2","category":"page"},{"location":"#Rematch2","page":"Home","title":"Rematch2","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Rematch2.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Rematch2]","category":"page"}]
+}
